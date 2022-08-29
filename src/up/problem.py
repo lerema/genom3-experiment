da09a2daa45ac691a1f9fc7d0760614bab716b1c
franklinselva <franklinselva10@gmail.com>
2022-08-29 14:52:26 +0200

    update up problem demo

--- src/up/problem.py
+++ src/up/problem.py
@@ -15,39 +15,73 @@ class VerifyStationProblem(object):
     def demo_problem(self):
         """Create a simple station verification application"""
         Location = UserType("Location")
+        Area = UserType("Area")
+
         robot_at = Fluent("robot_at", BoolType(), position=Location)
         verify_station_at = Fluent("verify_station_at", BoolType(), position=Location)
+        is_surveyed = Fluent("is_surveyed", BoolType(), area=Area)
+        is_location_surveyed = Fluent(
+            "is_location_surveyed", BoolType(), area=Area, position=Location
+        )
+        is_within_area = Fluent(
+            "is_within_area", BoolType(), area=Area, position=Location
+        )
 
-        move = InstantaneousAction("move", l_from=Location, l_to=Location)
+        move = InstantaneousAction("move", l_from=Location, l_to=Location, area=Area)
         l_from = move.parameter("l_from")
         l_to = move.parameter("l_to")
+        area = move.parameter("area")
+        move.add_precondition(is_surveyed(area))
+        move.add_precondition(is_location_surveyed(area, l_to))
         move.add_precondition(Not(Equals(l_from, l_to)))
         move.add_precondition(robot_at(l_from))
         move.add_precondition(Not(robot_at(l_to)))
         move.add_effect(robot_at(l_from), False)
         move.add_effect(robot_at(l_to), True)
 
-        capture_photo = InstantaneousAction("capture_photo", l=Location)
+        capture_photo = InstantaneousAction("capture_photo", l=Location, area=Area)
         l = capture_photo.parameter("l")
+        area = capture_photo.parameter("area")
+        capture_photo.add_precondition(is_surveyed(area))
+        capture_photo.add_precondition(is_location_surveyed(area, l))
         capture_photo.add_precondition(robot_at(l))
         capture_photo.add_effect(verify_station_at(l), True)
+
+        survey = InstantaneousAction("survey", area=Area)
+        area = survey.parameter("area")
+        survey.add_precondition(Not(is_surveyed(area)))
+        survey.add_effect(is_surveyed(area), True)
+
+        send_info = InstantaneousAction("send_info", area=Area, position=Location)
+        area = send_info.parameter("area")
+        position = send_info.parameter("position")
+        send_info.add_precondition(is_surveyed(area))
+        send_info.add_precondition(is_within_area(area, position))
+        send_info.add_effect(is_location_surveyed(area, position), True)
 
         l1 = Object("l1", Location)
         l2 = Object("l2", Location)
         l3 = Object("l3", Location)
         l4 = Object("l4", Location)
+        area = Object("area", Area)
 
         problem = Problem("robot")
         problem.add_fluent(robot_at)
         problem.add_fluent(verify_station_at)
+        problem.add_fluent(is_surveyed)
+        problem.add_fluent(is_location_surveyed)
+        problem.add_fluent(is_within_area)
 
         problem.add_action(move)
         problem.add_action(capture_photo)
+        problem.add_action(survey)
+        problem.add_action(send_info)
 
         problem.add_object(l1)
         problem.add_object(l2)
         problem.add_object(l3)
         problem.add_object(l4)
+        problem.add_object(area)
 
         problem.set_initial_value(robot_at(l1), True)
         problem.set_initial_value(robot_at(l2), False)
@@ -59,8 +93,26 @@ class VerifyStationProblem(object):
         problem.set_initial_value(verify_station_at(l3), False)
         problem.set_initial_value(verify_station_at(l4), False)
 
+        # Without actual preconditions, this is not a valid problem. It is only a
+        # placeholder for the demo.
+        problem.set_initial_value(is_surveyed(area), False)
+        problem.set_initial_value(is_within_area(area, l1), True)
+        problem.set_initial_value(is_within_area(area, l2), True)
+        problem.set_initial_value(is_within_area(area, l3), True)
+        problem.set_initial_value(is_within_area(area, l4), True)
+        problem.set_initial_value(is_location_surveyed(area, l1), False)
+        problem.set_initial_value(is_location_surveyed(area, l2), False)
+        problem.set_initial_value(is_location_surveyed(area, l3), False)
+        problem.set_initial_value(is_location_surveyed(area, l4), False)
+
+        problem.add_goal(is_surveyed(area))
+        problem.add_goal(is_location_surveyed(area, l1))
+        problem.add_goal(is_location_surveyed(area, l2))
+        problem.add_goal(is_location_surveyed(area, l3))
+        problem.add_goal(is_location_surveyed(area, l4))
+        problem.add_goal(verify_station_at(l1))
         problem.add_goal(verify_station_at(l2))
-        # problem.add_goal(verify_station_at(l3))
+        problem.add_goal(verify_station_at(l3))
         problem.add_goal(verify_station_at(l4))
         problem.add_goal(robot_at(l4))
 
--- src/up_demo.py
+++ src/up_demo.py
@@ -21,6 +21,14 @@ class UPBridge:
         "l3": {"x": 1.5, "y": -2.5, "z": 1.0, "yaw": 0.0},
         "l4": {"x": -1.5, "y": -3.5, "z": 1.0, "yaw": 0.0},
         "l5": {"x": 3.5, "y": 3.5, "z": 1.0, "yaw": 0.0},
+        "area": {
+            "xmin": -5.0,
+            "xmax": 5.0,
+            "ymin": -5.0,
+            "ymax": 5.0,
+            "z": 3.0,
+            "yaw": 0.0,
+        },
     }
     actions = {
         "move": Move,
@@ -28,6 +36,8 @@ class UPBridge:
         "stop": Stop,
         "takeoff": Takeoff,
         "capture_photo": None,  # TODO: implement this action
+        "survey": SurveyX,
+        "send_info": None,  # TODO: implement this action
     }
 
 
@@ -38,6 +48,7 @@ def main():
     problem = VerifyStationProblem().demo_problem()
     plan = None
     bridge = UPBridge()
+    action = Actions(connector.components)
 
     connector.start()
 
@@ -51,7 +62,7 @@ def main():
         plan = result.plan
 
     print("*** Executing plan ***")
-    result = Move(connector.components)(**bridge.objects["l1"])
+    result = action.move(l_from=None, l_to=bridge.objects["l1"])
     for timed_action in plan.timed_actions:
         action_instance = timed_action[1]
         print(action_instance)
@@ -61,16 +72,22 @@ def main():
         # TODO: Implement in a better way
         if str(action_name) == "move":
             result = drone_action(connector.components)(
-                **bridge.objects[str(parameter[-1])]
+                l_from=None, l_to=bridge.objects[str(parameter[0])]
             )
         elif str(action_name) == "capture_photo":
-            parameter = bridge.objects[str(parameter[-1])]
+            parameter = bridge.objects[str(parameter[0])]
             parameter["z"] = 0.15
-            result = Move(connector.components)(**parameter)
+            result = action.move(l_from=None, l_to=parameter)
             time.sleep(3)
-            result = Takeoff(connector.components)(height=1.0)
+            result = action.takeoff(height=1.0)
+        elif str(action_name) == "send_info":
+            parameter = bridge.objects[str(parameter[-1])]
+            print(f"Sending info to {parameter}")
+        elif str(action_name) == "survey":
+            parameter = bridge.objects[str(parameter[0])]
+            result = action.surveyx(**bridge.objects[str(parameter[0])])
         time.sleep(1)
-    result = Move(connector.components)(**bridge.objects["l1"])
+    result = action.move(l_from=None, l_to=bridge.objects["l1"])
     print("*** End of Execution ***")
 
     input("Press enter to exit...")
