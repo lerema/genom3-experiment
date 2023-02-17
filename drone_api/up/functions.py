2b25bd7e3a02fc9a2b055590563bd10873cf20ef
franklinselva <franklinselva10@gmail.com>
2023-02-17 16:04:29 +0100

    setup up components for querying with data

--- drone_api/up/functions.py
+++ drone_api/up/functions.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 """Functions to be called for estimating the drone and environment state."""
+from drone_api.up.user_types import Area, Location
 
 
 def get_battery_level(components):
@@ -68,3 +69,57 @@ def get_aruco_pose(components, marker):
 
     data = components["arucotag"].pose(str(marker))
     return data["pos"]
+
+
+class Fluents:
+    components = None
+
+    def robot_at(self, location: Location, **kwargs) -> bool:
+        assert isinstance(location, Location), f"{location} is not a Location"
+        result = get_robot_pose(self.components)
+        if not isinstance(location, dict):
+            location = location.__dict__()
+
+        def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
+            return abs(a - b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
+
+        for key in location:
+            if key in ["x", "y", "z"] and not isclose(result[key], location[key]):
+                return False
+
+        return True
+
+    def verify_station_at(self, location: Location, **kwargs) -> bool:
+        assert isinstance(location, Location), f"{location} is not a Location"
+
+        # TODO: Implement this function
+        # Check captured photo
+
+        return self.robot_at(location)
+
+    def is_surveyed(self, area: Area, **kwargs) -> bool:
+        # TODO: Implement this function with actual validataion
+        assert isinstance(area, Area), f"{area} is not a Area"
+        pass
+
+    def is_location_surveyed(self, area: Area, location: Location, **kwargs) -> bool:
+        # TODO: Implement this function with actual validataion
+        pass
+
+    def is_within_area(self, area: Area, location: Location, **kwargs) -> bool:
+        assert isinstance(area, Area), f"{area} is not a Area"
+        assert isinstance(location, Location), f"{location} is not a Location"
+        if not isinstance(area, dict):
+            area = area.__dict__()
+        if not isinstance(location, dict):
+            location = location.__dict__()
+
+        result = (area["xmin"] <= location["x"] <= area["xmax"]) and (
+            area["ymin"] <= location["y"] <= area["ymax"]
+        )
+        return bool(result)
+
+    def battery_at(self, level: int, **kwargs) -> bool:
+        assert isinstance(level, int), f"{level} is not an int"
+
+        return bool(get_battery_level(self.components) == level)
