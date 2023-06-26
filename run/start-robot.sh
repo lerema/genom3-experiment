688146342f9ab46f0228c9ab24f6ce6fa0d91bf0
franklinselva <franklinselva10@gmail.com>
2023-06-26 10:44:17 +0200

    setup dependencies for robot

--- run/start-robot.sh
+++ run/start-robot.sh
@@ -1,16 +1,39 @@
 #!/usr/bin/env bash
+logdir="/tmp/log-$(date +"%Y%m%d-%H%M%S")"
 SCRIPT_DIR="$(cd "$(dirname "$(realpath "$0")")" && pwd)"
-logdir="/log/log-$(date +"%Y%m%d-%H%M%S")"
+DATA_PATH="$SCRIPT_DIR"/../data
 
+# Load environment variables
+drone_experiment # Added in install-scripts/setup-experiment.sh
+
+# Source ROS workspace
+# shellcheck source=/dev/null
+source "${SCRIPT_DIR}"/../catkin_ws/devel/setup.bash
 mkdir -p "$logdir"
 rm -f core
-h2 init
+h2 init -d 500 -p 200000000
+
 genomixd -v -v >"$logdir"/genomixd.log &
+
+if [ $# -eq 0 ] || [ "$1" == "--python" ]; then
+    # Start Gazebo
+    user_cmd="echo \"Use this terminal to access the Python script\"; bash ; ${SCRIPT_DIR}/end.sh; rm $DATA_PATH/*; sleep 1"
+elif [ "$1" == "--tcl" ]; then
+    # Start Gazebo
+    user_cmd="script -f -c \"eltclsh -simu -cam -tf; ${SCRIPT_DIR}/end.sh; sleep 1\" $logdir/eltclsh.log"
+fi
+
 tmux \
-    new-session "nhfc-pocolibs -f -p |& tee -i $logdir/nhfc.log" \; \
-    split-window -p 66 "maneuver-pocolibs -f -p  |& tee -i $logdir/maneuver.log" \; \
-    split-window -p 50 "script -f -c \"eltclsh; ${SCRIPT_DIR}/end.sh; sleep 1\" $logdir/eltclsh.log" \; \
-    split-window -h -t 0 "rotorcraft-pocolibs -f -p |& tee -i $logdir/rotorcraft.log" \; \
-    split-window -h -t 2 "pom-pocolibs -f -p |& tee -i $logdir/pom.log" \; \
-    split-window -h -t 4 "optitrack-pocolibs -f -p |& tee -i $logdir/optitrack.log" \; \
+    new-session "roslaunch quad-cam_gazebo quad-cam.launch" \; \
+    split-window -p 66 "tf2-pocolibs -f |& tee -i $logdir/tf2.log" \; \
+    split-window -p 50 "CT_drone-pocolibs -f -i CT_drone1 |& tee -i $logdir/CT_drone.log" \; \
+    split-window -p 50 "arucotag-pocolibs -f -i arucotag1 |& tee -i $logdir/arucotag.log" \; \
+    split-window -h -t 2 "d435-pocolibs -f -i d4351 |& tee -i $logdir/arucotag.log" \; \
+    split-window -h -t 4 "camviz-pocolibs -f -i camviz1 |& tee -i $logdir/arucotag.log" \; \
+    new-window "optitrack-pocolibs -f |& tee -i $logdir/optitrack.log" \; \
+    split-window -p 66 "maneuver-pocolibs -f -i maneuver1 |& tee -i $logdir/maneuver.log" \; \
+    split-window -p 50 "$user_cmd" \; \
+    split-window -h -t 0 "nhfc-pocolibs -f -i nhfc1 |& tee -i $logdir/nhfc.log" \; \
+    split-window -h -t 2 "pom-pocolibs -f -i pom1 |& tee -i $logdir/pom.log" \; \
+    split-window -h -t 4 "rotorcraft-pocolibs -f -i rotorcraft1 |& tee -i $logdir/rotorcraft.log" \; \
     selectp -t 4
